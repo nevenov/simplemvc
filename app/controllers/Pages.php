87d6f8e65c9f11a4300992c5298a906b60435e2c
Stoyan Nedev <nevenov@abv.bg>
2019-08-26 16:34:06 +0300

    Loading Views in Controllers done.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,16 +1,18 @@
 <?php
-    class Pages extends Controller {
-
-        public function __construct() {
-            //echo "Pages Controlled loaded";
-        }
-
-        public function index(){
-
-        }
-
-        public function about($id) {
-            echo $id;
-        }
+class Pages extends Controller {
+    public function __construct(){
 
     }
+
+    public function index(){
+        $data = [
+            'title' => 'Welcome'
+        ];
+
+        $this->view('pages/index', $data);
+    }
+
+    public function about(){
+        $this->view('pages/about');
+    }
+}
